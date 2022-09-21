a5c7056d57db7751af7491c2d61e732c8c6a39ed
Nelson Quintanilla <nfquinta@gmail.com>
2022-09-21 14:37:37 +0100

    Generating logs_6

--- dissertation/testing.py
+++ dissertation/testing.py
@@ -1,10 +1,14 @@
+import random
+
 def test_f(start_, limit_, step_):
     for x in range(start_, limit_, step_):
         print(x)
 
 if __name__ == '__main__':
-    test_f(
-        start_=2,
-        limit_=15,
-        step_=2
-    )
+    # test_f(
+    #     start_=2,
+    #     limit_=15,
+    #     step_=2
+    # )
+
+    print(random.randint(1, 4))
